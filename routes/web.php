76960665ef69fe0570db3d131090c8ed021aca88
Ahsum1558 <ahsum1558@gmail.com>
2023-05-21 23:46:22 +0600

    Route reset

---
+++ routes/web.php
@@ -0,0 +1,177 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Super\SuperController;
+use App\Http\Controllers\Super\CmspageController;
+use App\Http\Controllers\Super\FieldarController;
+use App\Http\Controllers\Super\FieldbnController;
+use App\Http\Controllers\Super\FieldController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Admin Area Start
+Route::get('/', [AdminController::class, 'index'])->name('admin.home.index');
+// Admin Area End
+
+// Super Admin Page Start
+// Super Admin Area Start
+Route::prefix('/super')->group(function () {
+    Route::match(['get', 'post'], 'login', [SuperController::class, 'login'])->name('super.users.login');
+    Route::group(['middleware' => 'super'], function() {
+        Route::get('/', [SuperController::class, 'index'])->name('super.home.index');
+
+        // Logout Option
+        Route::get('/logout', [SuperController::class, 'superLogout'])->name('super.logout');
+
+        // Super Profile
+        Route::get('/profile', [SuperController::class, 'superProfile'])->name('super.profile');
+
+        // Profile Info Update
+        Route::get('/profile/info', [SuperController::class, 'superProfileInfo'])->name('super.profile.info');
+        Route::post('/profile/infoUpdate', [SuperController::class, 'superProfileInfoUpdate'])->name('super.profile.infoUpdate');
+
+        // Username Update
+        Route::get('/profile/username', [SuperController::class, 'superProfileUsername'])->name('super.profile.username');
+        Route::post('/profile/usernameUpdate', [SuperController::class, 'superProfileUsernameUpdate'])->name('super.profile.usernameUpdate');
+
+        // Email Update
+        Route::get('/profile/email', [SuperController::class, 'superProfileEmail'])->name('super.profile.email');
+        Route::post('/profile/emailUpdate', [SuperController::class, 'superProfileEmailUpdate'])->name('super.profile.emailUpdate');
+
+        // Photo Update
+        Route::get('/profile/image', [SuperController::class, 'superProfileImage'])->name('super.profile.image');
+        Route::post('/profile/imageUpdate', [SuperController::class, 'superProfileImageUpdate'])->name('super.profile.imageUpdate');
+
+        // Password Update
+        Route::get('/profile/password', [SuperController::class, 'superProfilePassword'])->name('super.profile.password');
+        Route::post('/profile/passwordUpdate', [SuperController::class, 'superProfilePasswordUpdate'])->name('super.profile.passwordUpdate');
+
+        // Theme Update
+        Route::get('/theme', [SuperController::class, 'superProfileTheme'])->name('super.theme');
+        Route::post('/themeUpdate', [SuperController::class, 'superProfileThemeUpdate'])->name('super.themeUpdate');
+    });
+});
+// Super Admin Area End
+
+// Meta Area Start
+Route::group(['middleware' => 'super'], function() {
+    // Super Meta Home Page
+    Route::get('/super/meta', [CmspageController::class, 'index'])->name('super.meta');
+    Route::post('/super/meta/store', [CmspageController::class, 'store'])->name('super.meta.store');
+
+    // Meta Info Update
+    Route::get('/super/meta/edit/{id}', [CmspageController::class, 'edit'])->name('super.meta.edit');
+    Route::post('/super/meta/update/{id}', [CmspageController::class, 'update'])->name('super.meta.update');
+
+    // Meta Delete
+    Route::get('/super/meta/destroy/{id}', [CmspageController::class, 'destroy'])->name('super.meta.destroy');
+});
+// Meta Area End
+
+// English Site Option Area Start
+Route::group(['middleware' => 'super'], function() {
+    // Super English Site Option Home Page
+    Route::get('/super/field', [FieldController::class, 'index'])->name('super.field');
+    Route::post('/super/field/store', [FieldController::class, 'store'])->name('super.field.store');
+
+    Route::get('/super/field/show/{id}', [FieldController::class, 'show'])->name('super.field.show');
+
+    // English Site Option Info Update
+    Route::get('/super/field/edit/{id}', [FieldController::class, 'edit'])->name('super.field.edit');
+    Route::post('/super/field/update/{id}', [FieldController::class, 'update'])->name('super.field.update');
+
+    // English Site Option Title Update
+    Route::get('/super/field/editTitle/{id}', [FieldController::class, 'editTitle'])->name('super.field.editTitle');
+    Route::post('/super/field/updateTitle/{id}', [FieldController::class, 'updateTitle'])->name('super.field.updateTitle');
+
+    // English Site Option Small Title Update
+    Route::get('/super/field/editSmallTitle/{id}', [FieldController::class, 'editSmallTitle'])->name('super.field.editSmallTitle');
+    Route::post('/super/field/updateSmallTitle/{id}', [FieldController::class, 'updateSmallTitle'])->name('super.field.updateSmallTitle');
+
+    // English Site Option License Update
+    Route::get('/super/field/editLicense/{id}', [FieldController::class, 'editLicense'])->name('super.field.editLicense');
+    Route::post('/super/field/updateLicense/{id}', [FieldController::class, 'updateLicense'])->name('super.field.updateLicense');
+
+    // English Site Option Logo Update
+    Route::get('/super/field/editLogo/{id}', [FieldController::class, 'editLogo'])->name('super.field.editLogo');
+    Route::post('/super/field/updateLogo/{id}', [FieldController::class, 'updateLogo'])->name('super.field.updateLogo');
+
+    // English Site Option Delete
+    Route::get('/super/field/destroy/{id}', [FieldController::class, 'destroy'])->name('super.field.destroy');
+});
+// English Site Option Area End
+
+// Arabic Site Option Area Start
+Route::group(['middleware' => 'super'], function() {
+    // Super Arabic Site Option Home Page
+    Route::get('/super/fieldar', [FieldarController::class, 'index'])->name('super.fieldar');
+    Route::post('/super/fieldar/store', [FieldarController::class, 'store'])->name('super.fieldar.store');
+
+    Route::get('/super/fieldar/show/{id}', [FieldarController::class, 'show'])->name('super.fieldar.show');
+
+    // Arabic Site Option Info Update
+    Route::get('/super/fieldar/edit/{id}', [FieldarController::class, 'edit'])->name('super.fieldar.edit');
+    Route::post('/super/fieldar/update/{id}', [FieldarController::class, 'update'])->name('super.fieldar.update');
+
+    // Arabic Site Option Title Update
+    Route::get('/super/fieldar/editTitle/{id}', [FieldarController::class, 'editTitle'])->name('super.fieldar.editTitle');
+    Route::post('/super/fieldar/updateTitle/{id}', [FieldarController::class, 'updateTitle'])->name('super.fieldar.updateTitle');
+
+    // Arabic Site Option License Update
+    Route::get('/super/fieldar/editLicense/{id}', [FieldarController::class, 'editLicense'])->name('super.fieldar.editLicense');
+    Route::post('/super/fieldar/updateLicense/{id}', [FieldarController::class, 'updateLicense'])->name('super.fieldar.updateLicense');
+
+    // Arabic Site Option Delete
+    Route::get('/super/fieldar/destroy/{id}', [FieldarController::class, 'destroy'])->name('super.fieldar.destroy');
+});
+// Arabic Site Option Area End
+
+// Bengali Site Option Area Start
+Route::group(['middleware' => 'super'], function() {
+    // Super Bengali Site Option Home Page
+    Route::get('/super/fieldbn', [FieldbnController::class, 'index'])->name('super.fieldbn');
+    Route::post('/super/fieldbn/store', [FieldbnController::class, 'store'])->name('super.fieldbn.store');
+
+    Route::get('/super/fieldbn/show/{id}', [FieldbnController::class, 'show'])->name('super.fieldbn.show');
+
+    // Bengali Site Option Info Update
+    Route::get('/super/fieldbn/edit/{id}', [FieldbnController::class, 'edit'])->name('super.fieldbn.edit');
+    Route::post('/super/fieldbn/update/{id}', [FieldbnController::class, 'update'])->name('super.fieldbn.update');
+
+    // Bengali Site Option Title Update
+    Route::get('/super/fieldbn/editTitle/{id}', [FieldbnController::class, 'editTitle'])->name('super.fieldbn.editTitle');
+    Route::post('/super/fieldbn/updateTitle/{id}', [FieldbnController::class, 'updateTitle'])->name('super.fieldbn.updateTitle');
+
+    // Bengali Site Option License Update
+    Route::get('/super/fieldbn/editLicense/{id}', [FieldbnController::class, 'editLicense'])->name('super.fieldbn.editLicense');
+    Route::post('/super/fieldbn/updateLicense/{id}', [FieldbnController::class, 'updateLicense'])->name('super.fieldbn.updateLicense');
+
+    // Bengali Site Option Delete
+    Route::get('/super/fieldbn/destroy/{id}', [FieldbnController::class, 'destroy'])->name('super.fieldbn.destroy');
+});
+// Bengali Site Option Area End
+
+// Super Admin Page End
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
